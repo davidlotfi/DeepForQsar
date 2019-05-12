8680d248412cb50643bd2e82e1f2e25638bf871c
abdellatifboukernine <31359561+davidlotfi@users.noreply.github.com>
2019-05-13 01:33:02 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import keras
+import tensorflow as tf
+import pandas as pd
+from keras import models
+from keras import layers
+
+
+#load data
+
+data=pd
+#premier definie architecture de modele
+
+#network = models.Sequential
+#network.add(layers.Dense(512,activation='relu',input_shape=(28*28,)))
+
+#compile le modele
+
+#network.compile(optimizer='rmsprop', loss='mse')
